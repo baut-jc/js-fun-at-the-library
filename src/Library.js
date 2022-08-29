2e485e3d116bca7a81f1ffae092e7e5bd2ca7217
Jocelle Bautista <jcllebautista@gmail.com>
2022-08-28 20:58:12 -0400

    edit Library test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,29 @@
+function createLibrary(name, type) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+      },
+    }
+  return library
+}
 
+function addBook(library, title) {
+  var genre = library.shelves
+  for (var i = 0; i < genre.length; i++)
+    genre.push(title)
+    console.log(genre)
+}
+
+// function checkOut(){
+//
+// }
+//library.shelves.genre
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook
 };
